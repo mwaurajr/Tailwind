8c3d2f7eb9ce20951cda9bc50ea400f446ab9744
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-04-23 01:43:11 +0300

    Final commit

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,77 +1,73 @@
-import React, { useState } from 'react'
-import { BsChevronCompactLeft } from 'react-icons/bs'
-import { BsChevronCompactRight } from 'react-icons/bs'
-import { RxDotFilled } from 'react-icons/rx'
+import React, { useState } from 'react';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
 
-function ImageSlider() {
+const ImageSlider = () => {
+  const slides = [
+    {
+      url: 'https://images.unsplash.com/photo-1464093515883-ec948246accb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2659&q=80',
+      title: 'Lobster',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1512132411229-c30391241dd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+      title: 'Sushi',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+      title: 'Pasta',
+    },
+    {
+      url: 'https://images.unsplash.com/photo-1539136788836-5699e78bfc75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+      title: 'Salmon',
+    },
+  ];
 
-    const slides = [
-        {
-            url: 'https://images.pexels.com/photos/7364105/pexels-photo-7364105.jpeg?auto=compress&cs=tinysrgb&w=600',
-            title: 'Lobster',
-        },
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-        {
-            url: 'https://images.pexels.com/photos/2323398/pexels-photo-2323398.jpeg?auto=compress&cs=tinysrgb&w=600',
-            title: 'Sushi',
-        },
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
 
-        {
-            url: 'https://media.istockphoto.com/id/1144823591/photo/spaghetti-in-a-dish-on-a-white-background.jpg?b=1&s=612x612&w=0&k=20&c=yWXnUB0TbGVWD5NE-dbu33KzQ_ujPXWQtU7809AUwxE=',
-            title: 'Pasta',
-        },
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
 
-        {
-            url: 'https://images.pexels.com/photos/2871757/pexels-photo-2871757.jpeg?auto=compress&cs=tinysrgb&w=600',
-            title: 'Salmon',
-        },
-    ];
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
 
-    const [currentIndex, setCurrentIndex] = useState(0)
+  return (
+    <div className='max-w-[1400px] h-[580px] w-full m-auto py-16 px-4 relative group'>
+      <div
+        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+      ></div>
+      {/* Left Arrow */}
+      <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className='absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
 
+      <div className='flex top-4 justify-center py-2'>
+        {slides.map((slide, slideIndex) => (
+          <div
+            className='text-2xl cursor-pointer'
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-    const prevSlide = () => {
-        const isFirstSlide = currentIndex === 0;
-        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-        setCurrentIndex(newIndex);
-    };
-
-    const nextSlide = () => {
-        const isLastSlide = currentIndex === slides.length - 1;
-        const newIndex = isLastSlide ? 0 : currentIndex + 1;
-        setCurrentIndex(newIndex);
-    };
-
-    const goToSlide = (slideIndex) => {
-        setCurrentIndex(slideIndex)
-    }
-    return (
-        <div className='max-width-[1400px] h-[580px] w-full m-auto py-16 px-4 relative group'>
-            <div className='w-full h-full bg-cover duration-500 bg-center rounded-2xl ' style={{ backgroundImage: `url(${slides[currentIndex].url})` }}>
-            </div>
-
-            {/* Left Arrow */}
-            <div className='absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer'>
-                <BsChevronCompactLeft onClick={prevSlide} size={30} />
-            </div>
-
-
-            {/* Right Arrow */}
-            <div className='absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer'>
-                <BsChevronCompactRight onClick={nextSlide} size={30} />
-            </div>
-
-            <div className='flex top-4 justify-center py-2'>
-                {slides.map((slide, slideIndex) => (
-                    <div className='cursor-pointer text-2xl' key={slideIndex} onClick={() => goToSlide(slideIndex)}>
-                        <RxDotFilled />
-                    </div>
-
-                ))}
-
-            </div>
-        </div>
-    )
-}
-
-export default ImageSlider
+export default ImageSlider;
